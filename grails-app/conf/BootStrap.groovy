f933249b314c435c8b3f9c2683927b5536dc25e3
code31415 <Ns7UJ35Hn>
2015-01-08 23:46:48 +0300

    picture

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,13 +1,20 @@
 import grails.util.Environment
 import ru.repo001.Book
+import ru.repo001.Picture
 
 class BootStrap {
 
     def init = { servletContext ->
         Environment.executeForCurrentEnvironment {
             development {
+                Picture picture = new Picture(height: 1, width: 1, size: 1)
+                picture.contentType = "image/jpeg"
+                picture.dir = "00/00/00/"
+                picture.fileName = "01.jpg"
+                picture.save()
+
                 30.times {
-                    Book book = new Book(name: "Повесть о ${it}", author: "Петров-${it} А.В.", active: true)
+                    Book book = new Book(name: "Повесть о ${it}", author: "Петров-${it} А.В.", active: true, picture: picture)
                     book.save(flush: true, failOnError: true)
                 }
             }
